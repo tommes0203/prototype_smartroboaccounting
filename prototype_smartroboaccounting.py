d20d8cc71935518e5c2779a1fd6150b411ea8a04
tommes0203 <thomas.lindner84@googlemail.com>
2021-07-26 09:58:28 +0200

    as

--- prototype_smartroboaccounting.py
+++ prototype_smartroboaccounting.py
@@ -1,5 +1,7 @@
 import boto3
 ddb = boto3.client("dynamodb")
+import boto3
+ddb = boto3.client("dynamodb")
 from ask_sdk_core.skill_builder import SkillBuilder
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.dispatch_components import AbstractExceptionHandler
@@ -25,6 +27,18 @@ class CatchAllExceptionHandler(AbstractExceptionHandler):
 class ChineseAnimalIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         return is_intent_name("ChineseAnimalIntent")(handler_input)
+        try:
+            data = ddb.get_item(
+                TableName="ChineseAnimal",
+                Key={
+                    'BirthYear': {
+                        'N': year
+                    }
+                }
+            )
+        except BaseException as e:
+            print(e)
+            raise(e)
         year = handler_input.request_envelope.request.intent.slots['year'].value
         
         try:
@@ -43,6 +57,21 @@ class ChineseAnimalIntentHandler(AbstractRequestHandler):
         speech_text = "Das gesuchte Tier ist ein " + data['Item']['Animal']['S'] + '. Möchtest du mehr wissen? Die Eigenschaften sind ' + data['Item']['PersonalityTraits']['S']
         handler_input.response_builder.speak(speech_text).set_should_end_session(False)
         return handler_input.response_builder.response      
+
+class getEmployeeCostCenterHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return is_intent_name("getEmployeeCostCenter")(handler_input)
+        empl_name = handler_input.request_envelope.request.intent.slots['empl_name'].value
+        
+
+        except BaseException as e:
+            print(e)
+            raise(e)
+
+        speech_text = "Das gesuchte Tier ist ein " + data['Item']['Animal']['S'] + '. Möchtest du mehr wissen? Die Eigenschaften sind ' + data['Item']['PersonalityTraits']['S']
+        handler_input.response_builder.speak(speech_text).set_should_end_session(False)
+        return handler_input.response_builder.response      
+
 
 sb = SkillBuilder()
 sb.add_request_handler(LaunchRequestHandler())
