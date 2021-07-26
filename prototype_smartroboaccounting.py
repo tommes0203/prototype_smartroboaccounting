fb2f77544211a142c13d980dc068066d8fca9715
tommes0203 <thomas.lindner84@googlemail.com>
2021-07-26 09:40:24 +0200

    Initiane

---
+++ prototype_smartroboaccounting.py
@@ -0,0 +1,51 @@
+import boto3
+ddb = boto3.client("dynamodb")
+from ask_sdk_core.skill_builder import SkillBuilder
+from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.dispatch_components import AbstractExceptionHandler
+from ask_sdk_core.utils import is_request_type, is_intent_name
+
+class LaunchRequestHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return is_request_type("LaunchRequest")(handler_input)
+
+    def handle(self, handler_input):
+        handler_input.response_builder.speak("Herzlich Willkommen ").set_should_end_session(False)
+        return handler_input.response_builder.response    
+
+class CatchAllExceptionHandler(AbstractExceptionHandler):
+    def can_handle(self, handler_input, exception):
+        return True
+
+    def handle(self, handler_input, exception):
+        print(exception)
+        handler_input.response_builder.speak("Ups there was an error, Thomas")
+        return handler_input.response_builder.response
+
+class ChineseAnimalIntentHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return is_intent_name("ChineseAnimalIntent")(handler_input)
+        try:
+            data = ddb.get_item(
+                TableName="ChineseAnimal",
+                Key={
+                    'BirthYear': {
+                        'N': year
+                    }
+                }
+            )
+        except BaseException as e:
+            print(e)
+            raise(e)
+
+        speech_text = "Your animal is a " + data['Item']['Animal']['S'] + '. Wanna know something else? Apparently you are ' + data['Item']['PersonalityTraits']['S']
+        handler_input.response_builder.speak(speech_text).set_should_end_session(False)
+        return handler_input.response_builder.response      
+
+sb = SkillBuilder()
+sb.add_request_handler(LaunchRequestHandler())
+sb.add_exception_handler(CatchAllExceptionHandler())
+sb.add_request_handler(ChineseAnimalIntentHandler())
+
+def handler(event, context):
+    return sb.lambda_handler()(event, context)
