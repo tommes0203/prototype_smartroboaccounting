631933ee0be56f2feacdfa0c208250521dda728f
tommes0203 <thomas.lindner84@googlemail.com>
2021-07-26 10:17:45 +0200

    Working_Package 1.0

--- prototype_smartroboaccounting.py
+++ prototype_smartroboaccounting.py
@@ -1,7 +1,5 @@
 import boto3
 ddb = boto3.client("dynamodb")
-import boto3
-ddb = boto3.client("dynamodb")
 from ask_sdk_core.skill_builder import SkillBuilder
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.dispatch_components import AbstractExceptionHandler
@@ -27,18 +25,8 @@ class CatchAllExceptionHandler(AbstractExceptionHandler):
 class ChineseAnimalIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         return is_intent_name("ChineseAnimalIntent")(handler_input)
-        try:
-            data = ddb.get_item(
-                TableName="ChineseAnimal",
-                Key={
-                    'BirthYear': {
-                        'N': year
-                    }
-                }
-            )
-        except BaseException as e:
-            print(e)
-            raise(e)
+    
+    def handle(self, handler_input):
         year = handler_input.request_envelope.request.intent.slots['year'].value
         
         try:
@@ -58,19 +46,19 @@ class ChineseAnimalIntentHandler(AbstractRequestHandler):
         handler_input.response_builder.speak(speech_text).set_should_end_session(False)
         return handler_input.response_builder.response      
 
-class getEmployeeCostCenterHandler(AbstractRequestHandler):
-    def can_handle(self, handler_input):
-        return is_intent_name("getEmployeeCostCenter")(handler_input)
-        empl_name = handler_input.request_envelope.request.intent.slots['empl_name'].value
-        
-
-        except BaseException as e:
-            print(e)
-            raise(e)
-
-        speech_text = "Das gesuchte Tier ist ein " + data['Item']['Animal']['S'] + '. Möchtest du mehr wissen? Die Eigenschaften sind ' + data['Item']['PersonalityTraits']['S']
-        handler_input.response_builder.speak(speech_text).set_should_end_session(False)
-        return handler_input.response_builder.response      
+#class getEmployeeCostCenterHandler(AbstractRequestHandler):
+#    def can_handle(self, handler_input):
+#        return is_intent_name("getEmployeeCostCenter")(handler_input)
+#        empl_name = handler_input.request_envelope.request.intent.slots['empl_name'].value
+#        
+#
+#        except BaseException as e:
+#            print(e)
+#            raise(e)
+#
+#        speech_text = "Das gesuchte Tier ist ein " + data['Item']['Animal']['S'] + '. Möchtest du mehr wissen? Die Eigenschaften sind ' + data['Item']['PersonalityTraits']['S']
+#        handler_input.response_builder.speak(speech_text).set_should_end_session(False)
+#        return handler_input.response_builder.response      
 
 
 sb = SkillBuilder()
