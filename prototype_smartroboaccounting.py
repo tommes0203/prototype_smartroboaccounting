e3ddbd7eb1e8e17294d29218d064e8e576a7cdf6
tommes0203 <thomas.lindner84@googlemail.com>
2021-08-12 13:38:45 +0200

    WORKING SUPPL CC 12.08.2021

--- prototype_smartroboaccounting.py
+++ prototype_smartroboaccounting.py
@@ -106,7 +106,7 @@ class getEmployeeLocationHandler(AbstractRequestHandler):
 # employee Location - ENDE
 
 
-# supplier volume - Test
+# supplier volume - WORKING
 class getSupplierValueHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         return is_intent_name("getSupplierValue")(handler_input)
@@ -128,13 +128,38 @@ class getSupplierValueHandler(AbstractRequestHandler):
             print(e)
             raise(e)
 
-        speech_text = "Beim Lieferanten " + data['Item']['suppl_name']['S'] + " sind im Jahr 2020 Produkte und Dienstleistungen im Wert von " + data['Item']['amount_2020']['S'] + ' eingekauft worden.';
+        speech_text = "Beim Lieferanten " + data['Item']['suppl_name']['S'] + " sind im aktuellen Jahr Produkte und Dienstleistungen im Wert von " + data['Item']['amount_2021']['S'] + " eingekauft worden. Im Jahr zuvor waren es " + data['Item']['amount_2020']['S'] + ". 2019 betrug der Warenumsatz " + data['Item']['amount_2019']['S'] ;
         handler_input.response_builder.speak(speech_text).set_should_end_session(False)
         return handler_input.response_builder.response
 
 # supplier volume - ENDE
 
+# supplier costcenter - TEST
+class getSupplierCostCenterHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        return is_intent_name("getSupplierCostCenter")(handler_input)
 
+    def handle(self, handler_input):
+        suppl_no = handler_input.request_envelope.request.intent.slots['intent_supplier_costcenter'].resolutions.resolutions_per_authority[0].values[0].value.id 
+
+        try:
+            data = ddb.get_item(
+                TableName="smart_supplier_costcenter",
+                Key={
+                    'suppl_id': {
+                        'N': suppl_no
+                    }
+                }
+            )
+        except BaseException as e:
+            print(e)
+            raise(e)
+
+        speech_text = "Lieferanten " + data['Item']['suppl_name']['S'] + " wurde insgesamt " + data['Item']['counter']['S'] + " mal auf Kostenstelle " + data['Item']['costcenter_no']['S'] + " gebucht.";
+        handler_input.response_builder.speak(speech_text).set_should_end_session(False)
+        return handler_input.response_builder.response
+
+# supplier costcenter - ENDE
 
 
 sb = SkillBuilder()
@@ -144,6 +169,7 @@ sb.add_request_handler(ChineseAnimalIntentHandler())
 sb.add_request_handler(getEmployeeCostCenterHandler())
 sb.add_request_handler(getEmployeeLocationHandler())
 sb.add_request_handler(getSupplierValueHandler())
+sb.add_request_handler(getSupplierCostCenterHandler())
 
 def handler(event, context):
     return sb.lambda_handler()(event, context)
